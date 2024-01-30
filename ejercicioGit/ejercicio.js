5ccc6628b978b24c9c3997f336a8f781d323ccc7
misaelChiluisa99 <chiluisajefferson97@gmail.com>
2024-01-29 23:33:46 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaaludo");
+    cmpImagenSaludo.src="./imagenes/imagen2.gif";
     console.log("ingresar a saludar");
 }

a79cb31b0ab01d9d265d173f047d102b9beab78b
misaelChiluisa99 <chiluisajefferson97@gmail.com>
2024-02-19 11:25:17 -0500

    habilitar componentens

--- 1-roles/roles/roles.js
+++ 1-roles/roles/roles.js
@@ -1,3 +1,4 @@
+let esNuevo=false;
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
     {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
@@ -9,6 +10,23 @@ mostrarOpcionEmpleado=function(){
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
     mostrarEmpleados();
+    
+    deshabilitarComponente("txtCedula");
+    deshabilitarComponente("txtNombre");
+    deshabilitarComponente("txtApellido");
+    deshabilitarComponente("txtSueldo");
+    deshabilitarComponente("btnGuardar");
+    
+}
+ejecutarNuevo=function(){
+    esNuevo=true;
+    habilitarComponente("txtCedula");
+    habilitarComponente("txtNombre");
+    habilitarComponente("txtApellido");
+    habilitarComponente("txtSueldo");
+    habilitarComponente("btnGuardar");
+
+
 }
 
 mostrarOpcionRol=function(){
@@ -45,6 +63,7 @@ mostrarEmpleados=function(){
     }
     contenidoTabla += "</table>";
     cmpTabla.innerHTML = contenidoTabla;
+
 
 }
 

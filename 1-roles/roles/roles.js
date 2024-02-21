1d09b37497adbbb73a08a95beeb5da42d55519e0
misaelChiluisa99 <chiluisajefferson97@gmail.com>
2024-02-20 19:06:05 -0500

    funcion guardarRol

--- 1-roles/roles/roles.js
+++ 1-roles/roles/roles.js
@@ -3,7 +3,12 @@ let empleados = [
     { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
     { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
     { cedula: "0202140810", nombre: "Misael", apellido: "Chiluisa", sueldo: 10000.0 }
+
 ]
+
+
+let roles = [];
+let rol = {};
 
 mostrarOpcionEmpleado = function () {
     mostrarComponente("divEmpleado");
@@ -37,6 +42,7 @@ mostrarOpcionRol = function () {
     mostrarComponente("divRol");
     ocultarComponente("divResumen");
     deshabilitarComponente("botonGuardarRol");
+    mostrarRoles();
 }
 
 mostrarOpcionResumen = function () {
@@ -143,12 +149,11 @@ guardar = function () {
             empleado.nombre = valorNombre;
             empleado.apellido = valorApellido;
             empleado.sueldo = valorSueldo;
-            let valorAgregarEmepleado = agregarEmpleado(empleado);
+            agregarEmpleado(empleado);
 
             deshabilitarCajasTexto();
             esNuevo = false;
         } else {
-      
             let empleadoEncontrado = buscarEmpleado(valorCedula);
             if (empleadoEncontrado != null) {
                 empleadoEncontrado.nombre = valorNombre;
@@ -165,86 +170,83 @@ guardar = function () {
 }
 
 
-limpiar=function(){
-    mostrarTextoEnCaja("txtCedula","");
-    mostrarTextoEnCaja("txtNombre","");
-    mostrarTextoEnCaja("txtApellido","");
-    mostrarTextoEnCaja("txtSueldo","");
-    esNuevo=false;
+limpiar = function () {
+    mostrarTextoEnCaja("txtCedula", "");
+    mostrarTextoEnCaja("txtNombre", "");
+    mostrarTextoEnCaja("txtApellido", "");
+    mostrarTextoEnCaja("txtSueldo", "");
+    esNuevo = false;
     deshabilitarCajasTexto();
 }
 
 
-buscarPorRol=function(){
-    let cedulaBuscar=recuperarTexto("txtBusquedaCedulaRol");
-    let empleadoBuscado=buscarEmpleado(cedulaBuscar);
-    if(empleadoBuscado!=null){
-        mostrarTexto("infoCedula",empleadoBuscado.cedula);
-        mostrarTexto("infoNombre",empleadoBuscado.nombre+" "+empleadoBuscado.apellido);
-        mostrarTexto("infoSueldo",empleadoBuscado.sueldo);
 
-    }else{
+calcularAporteEmpleado = function (sueldo) {
+    return sueldo * (9.45 / 100);
+}
+calcularValorAPagar = function (sueldo, aporte, descuento) {
+    return (sueldo - aporte - descuento);
+}
+
+calcularRol = function () {
+    let sueldoEmmpleado = recuperarFloatDiv("infoSueldo");
+    let descuentoEmpleado = recuperarFloat("txtDescuentos");
+    if (descuentoEmpleado >= 0 && descuentoEmpleado <= sueldoEmmpleado) {
+        let aporteEmpleado = calcularAporteEmpleado(sueldoEmmpleado);
+        mostrarTexto("infoIESS", aporteEmpleado.toFixed(2));
+        let valorAPagar = calcularValorAPagar(sueldoEmmpleado, descuentoEmpleado, aporteEmpleado);
+        mostrarTexto("infoPago", valorAPagar);
+        habilitarComponente("botonGuardarRol");
+
+    }
+}
+
+
+
+buscarPorRol = function () {
+    let cedulaBuscar = recuperarTexto("txtBusquedaCedulaRol");
+    let rolBuscado = buscarEmpleado(cedulaBuscar);
+    if (rolBuscado != null) {
+        mostrarTexto("infoCedula", rolBuscado.cedula);
+        mostrarTexto("infoNombre", rolBuscado.nombre + " " + rolBuscado.apellido);
+        mostrarTexto("infoSueldo", rolBuscado.sueldo);
+
+    } else {
         alert("No existe el empleado");
     }
 }
 
-calcularAporteEmpleado=function(sueldo){
-    return sueldo*(9.45/100);
-}
-calcularValorAPagar=function(sueldo,aporte,descuento){
-   return (sueldo-aporte-descuento);
-}
-
-calcularRol=function(){
-    let sueldoEmmpleado=recuperarFloatDiv("infoSueldo");
-    let descuentoEmpleado=recuperarFloat("txtDescuentos");
-    if(descuentoEmpleado>=0 && descuentoEmpleado<=sueldoEmmpleado){
-       let aporteEmpleado=calcularAporteEmpleado(sueldoEmmpleado);
-       mostrarTexto("infoIESS",aporteEmpleado.toFixed(2));
-      let valorAPagar=calcularValorAPagar(sueldoEmmpleado,descuentoEmpleado,aporteEmpleado);
-      mostrarTexto("infoPago",valorAPagar);
-      habilitarComponente("botonGuardarRol");
-      
-    }
-}
-
-let roles=[];
-let rol={ }
-
 buscarRol = function (cedula) {
     let elementoRol;
-    let RolEncontrado = null;
+    let rolEncontrado = null;
     for (let i = 0; i < roles.length; i++) {
         elementoRol = roles[i];
         if (elementoRol.cedula == cedula) {
-            RolEncontrado = elementoRol;
-            esNuevo = true;
-            break;
+            rolEncontrado = elementoRol;
         } else {
-            esNuevo = false;
         }
     }
-    return RolEncontrado;
+    return rolEncontrado;
 
 }
 
 
 agregarRol = function (rol) {
     let resultado;
-    resultado = buscarRol(roles.cedula);
+    resultado = buscarRol(rol.cedula);
     if (resultado == null) {
+        
         roles.push(rol);
         alert("rol agregado");
         mostrarRoles();
-
         return true;
     } else {
         alert("ya existe el rol con esa selda" + rol.cedula);
     } return false;
 }
 
-calcularAporteEmpleador=function(sueldo){
-    return((11.15/100)*sueldo);
+calcularAporteEmpleador = function (sueldo) {
+    return ((11.15 / 100) * sueldo);
 }
 
 
@@ -252,136 +254,44 @@ guardarRol = function () {
 
     let valorCedula = recuperarTextoDiv("infoCedula");
     let valorNombre = recuperarTextoDiv("infoNombre");
-    let valorSueldo = recuperarFloatDiv("infoSueldo");
-    let valorEmpleado=recuperarFloatDiv("infoIESS")
-    
-    let valorAporteEmpleador=calcularAporteEmpleador(valorSueldo);
-    let rol={ 
-        cedula:valorCedula,
-        nombre:valorNombre,
-        sueldo:valorSueldo,
+    let valorSueldo = recuperarTextoDiv("infoSueldo");
+    let valorEmpleado = recuperarTextoDiv("infoIESS")
+    let valorAporteEmpleador = calcularAporteEmpleador(valorSueldo);    
+    let rol = {
+        cedula: valorCedula,
+        nombre: valorNombre,
+        sueldo: valorSueldo,
         aporteEmpleado: valorEmpleado,
-        aporteEmpleador:valorAporteEmpleador,
-
-     };
-     agregarRol(rol);
-     alert("Exito");
-     deshabilitarComponente("botonGuardarRol");
-}
-
-
-
-/*
-buscarRol=function(cedula){
-    buscarPorRol=function(){
-        let cedulaBuscar=recuperarTexto("txtBusquedaCedulaRol");
-        let empleadoBuscado=buscarEmpleado(cedula);
-        if(empleadoBuscado!=null){
-            mostrarTexto("infoCedula",empleadoBuscado.cedula);
-            mostrarTexto("infoNombre",empleadoBuscado.nombre+" "+empleadoBuscado.apellido);
-            mostrarTexto("infoSueldo",empleadoBuscado.sueldo);
+        aporteEmpleador: valorAporteEmpleador,
+    };
     
-        }else{
-            alert("No existe el empleado");
-        }
-    }
-        
 
-}*/
-
-agregarRol=function(rolNuevo){
-
-    if(rolNuevo.cedula==roles.cedula){
-
-    }
+    agregarRol(rol);
+    alert("Exito");
+    deshabilitarComponente("botonGuardarRol");
+    mostrarRoles();
 }
 
-
-
-validarCedula = function (cedula) {
-    let cedulaValida = false;
-    if (cedula.length == 10) {
-        for (let i = 0; i < cedula.length; i++) {
-            cedulaValida = esDigito(cedula.charAt(i));
-            if (cedulaValida == false) {
-                mostrarTexto("lblErrorCedula", "Todos los numeros deben ser digitos");
-                return cedulaValida;
-            }
-        }
-    } else {
-        mostrarTexto("lblErrorCedula", "Debe tener 10 digitos");
-        return cedulaValida;
+mostrarRoles = function () {
+    let cmpTabla = document.getElementById("tablaResumen");
+    let contenidoTabla = "<table><tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>VALOR A PAGAR</th>" +
+        "<th>APORTE EMPLEADO</th>" +
+        "<th>APORTE EMPLEADOR</th>" +
+        "</tr>";
+    let elementoRol;
+    for (let i = 0; i < roles.length; i++) {
+        elementoRol = roles[i];
+        contenidoTabla +=
+            "<tr><td>" + elementoRol.cedula + "</td>"
+            + "<td>" + elementoRol.nombre + "</td>"
+            + "<td>" + elementoRol.sueldo + "</td>"
+            + "<td>" + elementoRol.aporteEmpleado + "</td>"
+            + "<td>" + elementoRol.aporteEmpleador + "</td>"
+            + "</tr>"
     }
-    mostrarTexto("lblErrorCedula", "");
-    return cedulaValida;
-}
-
-validarNombre = function (nombre) {
-    let nombreValido = false;
-    if (nombre.length >= 3) {
-        for (let i = 0; i < nombre.length; i++) {
-            nombreValido = esMayuscula(nombre.charAt(i));
-            if (nombreValido == false) {
-                mostrarTexto("lblErrorNombre", "Todo debe ser mayuscula");
-                return nombreValido;
-            }
-        }
-    } else {
-        mostrarTexto("lblErrorNombre", "Debe tener al menos 3 caracteres ");
-        return nombreValido;
-    }
-    mostrarTexto("lblErrorNombre", "");
-    return nombreValido;
-}
-
-
-
-validarApellido = function (apellido) {
-    let apellidoValido = false;
-    if (apellido.length >= 3) {
-        for (let i = 0; i < apellido.length; i++) {
-            apellidoValido = esMayuscula(apellido.charAt(i));
-            if (apellidoValido == false) {
-                mostrarTexto("lblErrorApellido", "Todo debe ser mayuscula");
-                return apellidoValido;
-            }
-        }
-    } else {
-        mostrarTexto("lblErrorApellido", "Debe tener al menos 3 caracteres ");
-        return apellidoValido;
-    }
-    mostrarTexto("lblErrorApellido", "");
-
-    return apellidoValido;
-}
-
-validarSueldo = function (sueldo) {
-    let sueldoValido = false;
-    if (sueldo >= 400 && sueldo <= 500) {
-        sueldoValido = true;
-        mostrarTexto("lblErrorNombre", "");
-        return sueldoValido;
-    } else {
-        mostrarTexto("lblErrorSueldo", "Debe tener entre 400 y 500 ");
-        return sueldoValido;
-    }
-}
-
-esMayuscula = function (caracter) {
-    let codigoLetra = caracter.charCodeAt(0);
-    if (codigoLetra >= 65 && codigoLetra <= 90) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-esDigito = function (caracter) {
-    let codigoLetra = caracter.charCodeAt(0);
-    if (codigoLetra >= 48 && codigoLetra <= 57) {
-        return true;
-    } else {
-        return false;
-    }
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
 }
